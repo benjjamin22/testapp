ffcdc48ec9926c6440f35e7dfc60b4aecee2b78d
benjamin <deeprising4ilife@gmail.com>
2024-04-04 08:42:03 -0800

    first commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/api', require('./routes'))
 app.use('/nams', require('./routes/nams'))
-app.use('/login', require('./routes/login'))
+    //app.use('/login', require('./routes/login'))
 app.use('/namslogin', require('./routes/NAMSLOGIN'))
 
 // Error handler middleware
